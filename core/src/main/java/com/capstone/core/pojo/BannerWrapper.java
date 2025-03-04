a2ec459a8f8a859e3855cf26d641fbc919c4f0a8
subkumar9 <subkumar9@publicisgroupe.net>
2025-03-04 13:50:36 +0530

    updated project structure

---
+++ core/src/main/java/com/capstone/core/pojo/BannerWrapper.java
@@ -0,0 +1,22 @@
+
+package com.capstone.core.pojo;
+import lombok.Setter;
+
+public class BannerWrapper {
+
+    @Setter
+    private String title;
+    @Setter
+    private String description;
+    @Setter
+    private String backgroundImage;
+    @Setter
+    private String altText;
+    @Setter
+    private String bannerCtaLabel;
+    @Setter
+    private Cta cta;
+    @Setter
+    private String alignment;
+    
+}
---
+++ core/src/main/java/com/capstone/core/pojo/Cta.java
@@ -0,0 +1,15 @@
+package com.capstone.core.pojo;
+
+import lombok.Setter;
+
+public class Cta {
+    
+  
+    @Setter
+    private String label;
+    
+    @Setter
+    private String url;
+
+
+}
