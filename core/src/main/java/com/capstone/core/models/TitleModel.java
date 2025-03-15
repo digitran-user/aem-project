de4f7efcacd32e69212fdddc615458ee5d41d2d7
subkumar9 <subkumar9@publicisgroupe.net>
2025-03-15 16:05:01 +0530

    changes for accordion and sliderbanner

--- core/src/main/java/com/capstone/core/models/TitleModel.java
+++ core/src/main/java/com/capstone/core/models/TitleModel.java
@@ -7,12 +7,11 @@ import javax.annotation.PostConstruct;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.capstone.core.pojo.BannerWrapper;
-import com.capstone.core.pojo.Cta;
 import com.capstone.core.pojo.TitleWrapper;
 import com.google.gson.Gson;
 
@@ -23,13 +22,29 @@ public class TitleModel {
    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
     @ValueMapValue(name="jcr:title")
+    @Optional
     private String title;
 
     @ValueMapValue
+    @Optional
     private String description;
 
     @ValueMapValue
+    @Optional
     private String alignment ;
+
+    @ValueMapValue
+    @Optional
+    private String titleFont;
+    
+    @ValueMapValue
+    @Optional
+    private String subtitleFont;
+
+    @ValueMapValue
+    @Optional
+    private String yspacing;
+    
      
     @PostConstruct
     protected void init() {
--- core/src/main/java/com/capstone/core/pojo/TitleWrapper.java
+++ core/src/main/java/com/capstone/core/pojo/TitleWrapper.java
@@ -14,5 +14,7 @@ public class TitleWrapper {
 
     private String alignment;
 
+    private Style style;
+
 
 }
