28d8b43c916f894dc77c8646fa3c76ca06fa0f2f
Ranjan <ranjanpandey2006@gmail.com>
2025-03-15 11:55:13 +0100

    fix

--- ui.frontend/src/stories/Video.stories.tsx
+++ ui.frontend/src/stories/Video.stories.tsx
@@ -17,7 +17,6 @@ export const Default: Story = {
   args: {
     videoUrl: "https://www.youtube.com/watch?v=6x5xtNhOts0",
     thumbnailUrl: "https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
-    videoTitle: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-    videoChannel: "youtube",
+    videoTitle: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
   }
 }
